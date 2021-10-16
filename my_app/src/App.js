5bfda32a71934010d5eca02c64c463e15066047d
yann-cloud <obamayann9@gmail.com>
2021-10-16 02:34:56 +0200

    project setup

---
+++ my_app/src/App.js
@@ -0,0 +1,65 @@
+
+  import React, { useState, useEffect } from "react";
+  import "./App.css";
+  import io from "socket.io-client";
+
+  const socket = io();
+
+  function App() {
+    const [message, setMessage] = useState("");
+    const [receivedMessages, setReceivedMessages] = useState([]);
+    const [displayName, setDisplayName] = useState(null);
+    const [displayNameInput, setDisplayNameInput] = useState("");
+
+    useEffect(() => {
+      socket.on("message", message => {
+        setReceivedMessages(prevState => [...prevState, message]);
+      });
+    }, []);
+
+    const sendMessage = () => {
+      socket.emit("message", `${displayName}: ${message}`);
+      setMessage("");
+    };
+
+    const onUpdateMessage = event => {
+      setMessage(event.target.value);
+    };
+
+    const onUpdateDisplayNameInput = event => {
+      setDisplayNameInput(event.target.value);
+    };
+
+    const updateDisplayName = () => {
+      setDisplayName(displayNameInput);
+    };
+
+    return (
+      <div className="container">
+        {displayName ? (
+          <>
+            <ul>
+              {receivedMessages.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+            <input type="text" value={message} onChange={onUpdateMessage} />
+            <button onClick={sendMessage}>SEND</button>
+          </>
+        ) : (
+          <>
+            <label for="display-name">Display Name</label>
+            <br />
+            <input
+              type="text"
+              value={displayNameInput}
+              onChange={onUpdateDisplayNameInput}
+            />
+            <button onClick={updateDisplayName}>SUBMIT</button>
+          </>
+        )}
+      </div>
+    );
+  }
+
+  export default App;
